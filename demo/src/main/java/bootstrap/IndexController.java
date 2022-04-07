695d1564af4a7205d1fbbe482a031409ecfeda7b
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2022-04-07 17:40:19 +0200

    Add files via upload
    RequestMapping

--- demo/src/main/java/bootstrap/IndexController.java
+++ demo/src/main/java/bootstrap/IndexController.java
@@ -2,13 +2,16 @@ package bootstrap;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@EnableAutoConfiguration
+
 @Controller
+@RequestMapping("/")
 public class IndexController {
 
-	@RequestMapping("/")
+	@GetMapping(value = "/hw")
 	public String Index()
 	{
 		return "index";
