ede5890274c8a6d58da5707e21d15aee8bf72914
Jonaeffm <86629759+Jonaeffm@users.noreply.github.com>
2023-01-19 18:02:51 +0100

    Add files via upload

--- demo/src/main/java/bootstrap/WebSecurityConfig.java
+++ demo/src/main/java/bootstrap/WebSecurityConfig.java
@@ -34,11 +34,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
    protected void configure(HttpSecurity http) throws Exception {
 	   http.csrf().disable()
            .authorizeRequests()
+           .antMatchers("/addUser").anonymous()
                .anyRequest().authenticated()
                .and()
            .formLogin()
                .loginPage("/login")
                .permitAll();
+	   
    }
    
    @SuppressWarnings("deprecation")
